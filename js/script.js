adb0413592ce4e5de4e5c0d5a119fc0f63e16eb9
Marthen88 <dev.marcelojs@gmail.com>
2021-12-31 11:21:48 -0300

    home

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var tela = document.getElementById('home')
+
+function aboutme(){
+    tela.innerHTML = 'Meu nome é Marcelo José da Silva, sou casado'
+    tela.style.background = 'purple'
+    tela.style.fontFamily = 'consolas'
+    tela.style.fontSize = '30px'
+    tela.style.color = 'white'
+}
